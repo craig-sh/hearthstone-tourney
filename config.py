86e919cff172e34e987e5b14a91aaf9d73b642bd
craig-sh <craig.henriques@mail.utoronto.ca>
2015-08-04 22:16:21 -0400

    Added cities models. Multiple cities exist in a region

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+# create a file called secret_key.py in the same folder as config.py
+# and declare the variable:
+# secret_key = 'some-secret-key'
+from secret_key import secret_key
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = secret_key
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DB_FILE_PATH = os.path.join('/tmp', 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DB_FILE_PATH
