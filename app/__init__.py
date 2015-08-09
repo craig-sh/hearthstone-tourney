31449fda8cd6a684c63779a3b59dd9f2a2c5f078
craig-sh <craig.henriques@mail.utoronto.ca>
2015-08-09 19:59:42 -0400

    Only recreate db if db file doesn't exist

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,61 @@ from collections import deque
 import os
 
 
+def init_db():
+    regions = deque()
+    regions.append(models.Region('terra', [2]))  # 1
+    regions.append(models.Region('markee', [1]))  # 2
+    regions.append(models.Region('heim', [1, 2]))  # 3
+
+    for region in regions:
+        db.session.add(region)
+    db.session.flush()
+
+    cities = deque()
+    cities.append(models.City('steepes', 1))
+    cities.append(models.City('danggg', 1))
+    cities.append(models.City('planes', 2))
+    cities.append(models.City('forest', 2))
+    cities.append(models.City('castle', 3))
+    cities.append(models.City('toilet', 3))
+
+    for city in cities:
+        db.session.add(city)
+    db.session.flush()
+
+    players = deque()
+    players.append(models.Player('Kai1'))
+    players.append(models.Player('Kai2'))
+    players.append(models.Player('Kai3'))
+
+    for player in players:
+        db.session.add(player)
+    db.session.flush()
+
+    classes = deque()
+    classes.append(models.Hsclass('Priest'))
+    classes.append(models.Hsclass('Warrior'))
+    classes.append(models.Hsclass('Shaman'))
+
+    for hsclass in classes:
+        db.session.add(hsclass)
+    db.session.flush()
+
+    city_states = deque()
+    city_states.append(models.CityState(1, 1))
+    city_states.append(models.CityState(2, 1))
+    city_states.append(models.CityState(3, 2))
+    city_states.append(models.CityState(4, 3))
+    city_states.append(models.CityState(5, 2))
+    city_states.append(models.CityState(6, 3))
+
+    for state in city_states:
+        db.session.add(state)
+    db.session.flush()
+
 app = Flask(__name__)
 app.config.from_object('config')
-try:
-    os.remove(app.config['DB_FILE_PATH'])
-except FileNotFoundError:
-    pass
+
 
 db = SQLAlchemy(app)
 # the app in the import statement below is not the same
@@ -18,58 +67,10 @@ db = SQLAlchemy(app)
 # and assigning the Flask instance to app
 from app import views, models
 
+db_exists = os.path.exists(app.config['DB_FILE_PATH'])
 db.create_all()
 
-regions = deque()
-regions.append(models.Region('terra', [2]))  # 1
-regions.append(models.Region('markee', [1]))  # 2
-regions.append(models.Region('heim', [1, 2]))  # 3
-
-for region in regions:
-    db.session.add(region)
-db.session.flush()
-
-cities = deque()
-cities.append(models.City('steepes', 1))
-cities.append(models.City('danggg', 1))
-cities.append(models.City('planes', 2))
-cities.append(models.City('forest', 2))
-cities.append(models.City('castle', 3))
-cities.append(models.City('toilet', 3))
-
-for city in cities:
-    db.session.add(city)
-db.session.flush()
-
-
-players = deque()
-players.append(models.Player('Kai1'))
-players.append(models.Player('Kai2'))
-players.append(models.Player('Kai3'))
-
-for player in players:
-    db.session.add(player)
-db.session.flush()
-
-classes = deque()
-classes.append(models.Hsclass('Priest'))
-classes.append(models.Hsclass('Warrior'))
-classes.append(models.Hsclass('Shaman'))
-
-for hsclass in classes:
-    db.session.add(hsclass)
-db.session.flush()
-
-city_states = deque()
-city_states.append(models.CityState(1, 1))
-city_states.append(models.CityState(2, 1))
-city_states.append(models.CityState(3, 2))
-city_states.append(models.CityState(4, 3))
-city_states.append(models.CityState(5, 2))
-city_states.append(models.CityState(6, 3))
-
-for state in city_states:
-    db.session.add(state)
-db.session.flush()
+if not db_exists:
+    init_db()
 
 db.session.commit()
